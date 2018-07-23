a7df7153286449766d2044f7404f57b7cf43a7ef
lhassler <lhassler@SEW-01489-LHH>
2018-07-23 12:03:14 +0200

    added polygons

--- WindowsFormsApp1/GraphicTest.cs
+++ WindowsFormsApp1/GraphicTest.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -13,10 +14,14 @@ namespace WindowsFormsApp1
     {
         private Pen penWhite = new Pen(Color.White);
         private Stopwatch sw;
-        private List<Point> lines = new List<Point>();
-        static Random rnd = new Random();
-
         private int lastPaint = 0;
+        static Random rnd = new Random();
+        private System.Timers.Timer time = new System.Timers.Timer();
+
+        // Line List
+        private List<Point> lines = new List<Point>();
+        // Polygon List
+        private List<MovingPolygon> polys = new List<MovingPolygon>();
 
         public GraphicTest()
         {
@@ -27,7 +32,26 @@ namespace WindowsFormsApp1
             DoubleBuffered = true;
             sw = new Stopwatch();
             sw.Start();
-            lines.Add(new Point(0,0));
+
+            // Timer
+            time.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            time.Interval = 1000;
+            time.Enabled = true;
+
+            // Line
+            // lines.Add(new Point(0,0));
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            int count = rnd.Next(3, 8);
+            PointF[] points = new PointF[count];
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new PointF(rnd.Next(50,500), rnd.Next(50,200));
+            }
+
+            polys.Add(new MovingPolygon(points, rnd.Next(4) - 2, rnd.Next(4) - 2));
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -35,9 +59,36 @@ namespace WindowsFormsApp1
             int timeOffset = (int)sw.ElapsedMilliseconds - lastPaint;
             lastPaint = (int) sw.ElapsedMilliseconds;
             e.Graphics.DrawString("T Offs:"+Convert.ToString(timeOffset), new Font("system", 10), Brushes.White, 400, 30);
-
             e.Graphics.DrawString("Time:  "+Convert.ToString(sw.ElapsedMilliseconds), new Font("system", 10), Brushes.White, 400, 10);
-            e.Graphics.DrawString("List Objects: "+Convert.ToString(lines.Count), new Font("system", 10), Brushes.White, 400, 50);
+
+            //DrawLines(e);
+            DrawPolygons(e);
+            Invalidate();
+        }
+
+        private void DrawPolygons(PaintEventArgs e)
+        {
+            try
+            {
+                foreach (MovingPolygon pol in polys)
+                {
+                    e.Graphics.DrawPolygon(penWhite, pol.Points);
+                    if (sw.ElapsedMilliseconds%5 ==0)
+                        pol.Move();
+                }
+            }
+            catch (Exception exception)
+            {
+
+            }
+            
+            
+        }
+
+        private void DrawLines(PaintEventArgs e)
+        {
+            e.Graphics.DrawString("List Objects: " + Convert.ToString(lines.Count), new Font("system", 10), Brushes.White, 400,
+                50);
 
             for (int i = 0; i < lines.Count; i++)
             {
@@ -47,11 +98,41 @@ namespace WindowsFormsApp1
                 }
                 else
                 {
-                    lines[lines.Count - 1] = new Point(lines[lines.Count - 1].X + rnd.Next(20), lines[lines.Count - 1].Y + rnd.Next(20));
+                    lines[lines.Count - 1] = new Point(lines[lines.Count - 1].X + rnd.Next(20),
+                        lines[lines.Count - 1].Y + rnd.Next(20));
                 }
+
                 e.Graphics.DrawLine(penWhite, 0, 0, lines[i].X, lines[i].Y);
             }
-            Invalidate();
         }
+    }
+
+    class MovingPolygon
+    {
+        private PointF[] points;
+        private int speedX;
+        private int speedY;
+
+        public PointF[] Points { get => points; set => points = value; }
+        public int SpeedX { get => speedX; set => speedX = value; }
+        public int SpeedY { get => speedY; set => speedY = value; }
+
+
+        public MovingPolygon(PointF[] points, int sX, int sY)
+        {
+            this.Points = points;
+            SpeedX = sX;
+            SpeedY = sY;
+        }
+
+        public void Move()
+        {
+            for (int i=0; i<points.Length; i++)
+            {
+                points[i] = new PointF(points[i].X+SpeedX,points[i].Y+speedY);
+            }
+        }
+
+
     }
 }
