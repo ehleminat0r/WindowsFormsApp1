56fd968fb77295cc65076d7a77a31be469435ff3
lhassler <lhassler@SEW-01489-LHH>
2018-08-02 12:01:39 +0200

    added sound pattern matrix

--- WindowsFormsApp1/MusicMachine.cs
+++ WindowsFormsApp1/MusicMachine.cs
@@ -4,6 +4,10 @@ using System.Drawing.Drawing2D;
 using System.Media;
 using System.Timers;
 using System.Windows.Forms;
+using System.Windows.Media;
+using Brushes = System.Drawing.Brushes;
+using Color = System.Drawing.Color;
+using Pen = System.Drawing.Pen;
 
 
 namespace WindowsFormsApp1
@@ -11,15 +15,23 @@ namespace WindowsFormsApp1
 
     class MusicMachine : Form
     {
+        // Timer (atm not necessary)
         System.Timers.Timer time = new System.Timers.Timer();
+
+        // Music patern
         bool[] sound1 = new bool[16];
+        bool[] sound2 = new bool[16];
+        bool[] sound3 = new bool[16];
+
+        // MediaPlayers
+        MediaPlayer player1 = new MediaPlayer();
+        MediaPlayer player2 = new MediaPlayer();
+        MediaPlayer player3 = new MediaPlayer();
+
+        // Time position
         private int pos = 0;
-        private Pen pen;
 
-        // Soundplayer
-        System.Media.SoundPlayer player = new SoundPlayer();
-
-        // Textbox
+        // Textbox (speed)
         TextBox tb = new TextBox();
 
         public MusicMachine()
@@ -36,15 +48,7 @@ namespace WindowsFormsApp1
             // Initialize Timer
             time.Interval = 1000;
             time.Elapsed += new ElapsedEventHandler(OnTick);
-            time.Start();
-
-            //soundplayer
-            player.SoundLocation = @"C:\windows\media\Windows Default.wav";
-            player.Load();
-
-            // Pen
-            pen = new Pen(Color.DarkBlue, 5);
-            pen.StartCap = LineCap.ArrowAnchor;
+            //time.Start();
 
             // Textbox
             tb.Left = 20;
@@ -52,7 +56,6 @@ namespace WindowsFormsApp1
             tb.Width = 40;
             tb.Height = 80;
             tb.Text = "5";
-
             this.Controls.Add(tb);
         }
 
@@ -89,59 +92,113 @@ namespace WindowsFormsApp1
             // play sound
             if (sound1[pos / 15] == true && pos % 15 == 0)
             {
-                player.Play();
+                player1.Open(new System.Uri(@"C:\windows\media\Windows Default.wav"));
+                player1.Play();
+            }
+            if (sound2[pos / 15] == true && pos % 15 == 0)
+            {
+                player2.Open(new System.Uri(@"C:\windows\media\chord.wav"));
+                player2.Play();
+            }
+            if (sound3[pos / 15] == true && pos % 15 == 0)
+            {
+                player3.Open(new System.Uri(@"C:\windows\media\ding.wav"));
+                player3.Play();
             }
 
+
             // Border
-             e.Graphics.DrawRectangle(Pens.Black, 10, 10, 250, 340);
+            e.Graphics.DrawRectangle(Pens.Black, 10, 10, 250, 340);
 
             // Draw Time Position
-            e.Graphics.DrawLine(pen, 15 + pos, 45, 15 + pos, 70);
+            e.Graphics.DrawLine(Pens.Black, 15 + pos, 15, 15 + pos, 200);
 
-            // Draw Sound bar
-            for (int i = 0; i < sound1.Length; i++)
+            // Draw Sound bar 1
+            DrawPattern(e, sound1, 1);
+
+            // Draw Sound bar 2
+            DrawPattern(e, sound2, 2);
+
+            // Draw Sound bar 3
+            DrawPattern(e, sound3, 3);
+
+            Invalidate();
+        }
+
+        private void DrawPattern(PaintEventArgs e, bool[] sound, int row)
+        {
+            for (int i = 0; i < sound.Length; i++)
             {
-                if (sound1[i] == false)
+                if (sound[i] == false)
                 {
                     if (i % 4 != 0)
                     {
-                        e.Graphics.DrawRectangle(Pens.Black, 15 + i * 15, 20, 9, 20);
+                        e.Graphics.DrawRectangle(Pens.Black, 15 + i * 15, row * 40 - 15, 9, 20);
                     }
                     else
                     {
-                        e.Graphics.DrawRectangle(Pens.Red, 15 + i * 15, 20, 9, 20);
+                        e.Graphics.DrawRectangle(Pens.Red, 15 + i * 15, row * 40 - 15, 9, 20);
                     }
-                    
                 }
                 else
                 {
                     if (i % 4 != 0)
                     {
-                        e.Graphics.FillRectangle(Brushes.Black, 15 + i * 15, 20, 9, 20);
+                        e.Graphics.FillRectangle(Brushes.Black, 15 + i * 15, row * 40 - 15, 9, 20);
                     }
                     else
                     {
-                        e.Graphics.FillRectangle(Brushes.Red, 15 + i * 15, 20, 9, 20);
+                        e.Graphics.FillRectangle(Brushes.Red, 15 + i * 15, row * 40 - 15, 9, 20);
                     }
                 }
             }
-            e.Graphics.FillRectangle(Brushes.Blue, 15 + (pos/15) * 15, 20, 9, 20);
 
-            Invalidate();
+            e.Graphics.FillRectangle(Brushes.Blue, 15 + (pos / 15) * 15, row * 40 - 15, 9, 20);
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
             try
             {
-                if (sound1[(e.X - 15) / 15] == false)
+                // sound pattern 1
+                if (e.Y >= 26 && e.Y <= 46)
                 {
-                    sound1[(e.X - 15) / 15] = true;
+                    if (sound1[(e.X - 15) / 15] == false)
+                    {
+                        sound1[(e.X - 15) / 15] = true;
+                    }
+                    else
+                    {
+                        sound1[(e.X - 15) / 15] = false;
+                    }
                 }
-                else
+
+                // sound pattern 2
+                if (e.Y >= 66 && e.Y <= 86)
                 {
-                    sound1[(e.X - 15) / 15] = false;
+                    if (sound2[(e.X - 15) / 15] == false)
+                    {
+                        sound2[(e.X - 15) / 15] = true;
+                    }
+                    else
+                    {
+                        sound2[(e.X - 15) / 15] = false;
+                    }
                 }
+
+                // sound pattern 3
+                if (e.Y >= 106 && e.Y <= 126)
+                {
+                    if (sound3[(e.X - 15) / 15] == false)
+                    {
+                        sound3[(e.X - 15) / 15] = true;
+                    }
+                    else
+                    {
+                        sound3[(e.X - 15) / 15] = false;
+                    }
+                }
+
             }
             catch (Exception exception)
             {
