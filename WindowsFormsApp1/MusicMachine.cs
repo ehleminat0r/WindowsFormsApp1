7ce9412392473a58adcc46ba836b997964b45fdd
lhassler <lhassler@SEW-01489-LHH>
2018-08-02 14:32:18 +0200

    added on off buttons

--- WindowsFormsApp1/MusicMachine.cs
+++ WindowsFormsApp1/MusicMachine.cs
@@ -5,6 +5,7 @@ using System.Media;
 using System.Timers;
 using System.Windows.Forms;
 using System.Windows.Media;
+using Brush = System.Drawing.Brush;
 using Brushes = System.Drawing.Brushes;
 using Color = System.Drawing.Color;
 using Pen = System.Drawing.Pen;
@@ -18,15 +19,20 @@ namespace WindowsFormsApp1
         // Timer (atm not necessary)
         System.Timers.Timer time = new System.Timers.Timer();
 
-        // Music patern
+        // Music pattern
         bool[] sound1 = new bool[16];
         bool[] sound2 = new bool[16];
         bool[] sound3 = new bool[16];
+        bool[] sound4 = new bool[16];
+
+        // Pattern on off
+        private bool[] pattern = {true, true, true, true};
 
         // MediaPlayers
         MediaPlayer player1 = new MediaPlayer();
         MediaPlayer player2 = new MediaPlayer();
         MediaPlayer player3 = new MediaPlayer();
+        MediaPlayer player4 = new MediaPlayer();
 
         // Time position
         private int pos = 0;
@@ -36,7 +42,7 @@ namespace WindowsFormsApp1
 
         public MusicMachine()
         {
-            this.Size = new Size(285, 400);
+            this.Size = new Size(310, 400);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MinimizeBox = false;
             this.MaximizeBox = false;
@@ -49,7 +55,7 @@ namespace WindowsFormsApp1
             time.Interval = 1000;
             time.Elapsed += new ElapsedEventHandler(OnTick);
             //time.Start();
-
+            
             // Textbox
             tb.Left = 20;
             tb.Top = 320;
@@ -64,6 +70,7 @@ namespace WindowsFormsApp1
         private void OnTick(Object sender, ElapsedEventArgs e)
         {
             //Console.WriteLine(e.SignalTime);
+
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -90,25 +97,29 @@ namespace WindowsFormsApp1
             }
 
             // play sound
-            if (sound1[pos / 15] == true && pos % 15 == 0)
+            if (sound1[pos / 15] == true && pos % 15 == 0 && pattern[0] == true)
             {
                 player1.Open(new System.Uri(@"C:\windows\media\Windows Default.wav"));
                 player1.Play();
             }
-            if (sound2[pos / 15] == true && pos % 15 == 0)
+            if (sound2[pos / 15] == true && pos % 15 == 0 && pattern[1] == true)
             {
                 player2.Open(new System.Uri(@"C:\windows\media\chord.wav"));
                 player2.Play();
             }
-            if (sound3[pos / 15] == true && pos % 15 == 0)
+            if (sound3[pos / 15] == true && pos % 15 == 0 && pattern[2] == true)
             {
                 player3.Open(new System.Uri(@"C:\windows\media\ding.wav"));
                 player3.Play();
             }
-
+            if (sound4[pos / 15] == true && pos % 15 == 0 && pattern[3] == true)
+            {
+                player4.Open(new System.Uri(@"C:\windows\media\Windows Ding.wav"));
+                player4.Play();
+            }
 
             // Border
-            e.Graphics.DrawRectangle(Pens.Black, 10, 10, 250, 340);
+            e.Graphics.DrawRectangle(Pens.Black, 10, 10, 280, 340);
 
             // Draw Time Position
             e.Graphics.DrawLine(Pens.Black, 15 + pos, 15, 15 + pos, 200);
@@ -121,6 +132,22 @@ namespace WindowsFormsApp1
 
             // Draw Sound bar 3
             DrawPattern(e, sound3, 3);
+
+            // Draw Sound bar 4
+            DrawPattern(e, sound4, 4);
+
+            // Draw switch
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] == false)
+                {
+                    e.Graphics.DrawEllipse(Pens.Black, 260, 25 + i * 40, 20, 20);
+                }
+                else
+                {
+                    e.Graphics.FillEllipse(Brushes.LightGreen, 260, 25+i * 40, 20, 20);
+                }
+            }
 
             Invalidate();
         }
@@ -153,49 +180,127 @@ namespace WindowsFormsApp1
                 }
             }
 
-            e.Graphics.FillRectangle(Brushes.Blue, 15 + (pos / 15) * 15, row * 40 - 15, 9, 20);
+            if (pattern[row - 1] == true)
+            {
+                e.Graphics.FillRectangle(Brushes.Blue, 15 + (pos / 15) * 15, row * 40 - 15, 9, 20);
+            }
+            
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
+            //Console.WriteLine(e.X);
             try
             {
                 // sound pattern 1
                 if (e.Y >= 26 && e.Y <= 46)
                 {
-                    if (sound1[(e.X - 15) / 15] == false)
+                    if (e.X <= 250)
                     {
-                        sound1[(e.X - 15) / 15] = true;
+                        if (sound1[(e.X - 15) / 15] == false)
+                        {
+                            sound1[(e.X - 15) / 15] = true;
+                        }
+                        else
+                        {
+                            sound1[(e.X - 15) / 15] = false;
+                        }
                     }
-                    else
+
+                    if (e.X >= 260 && e.X <= 280)
                     {
-                        sound1[(e.X - 15) / 15] = false;
+                        if (pattern[0] == true)
+                        {
+                            pattern[0] = false;
+                        }
+                        else
+                        {
+                            pattern[0] = true;
+                        }
                     }
                 }
 
                 // sound pattern 2
                 if (e.Y >= 66 && e.Y <= 86)
                 {
-                    if (sound2[(e.X - 15) / 15] == false)
+                    if (e.X <= 250)
                     {
-                        sound2[(e.X - 15) / 15] = true;
+                        if (sound2[(e.X - 15) / 15] == false)
+                        {
+                            sound2[(e.X - 15) / 15] = true;
+                        }
+                        else
+                        {
+                            sound2[(e.X - 15) / 15] = false;
+                        }
                     }
-                    else
+
+                    if (e.X >= 260 && e.X <= 280)
                     {
-                        sound2[(e.X - 15) / 15] = false;
+                        if (pattern[1] == true)
+                        {
+                            pattern[1] = false;
+                        }
+                        else
+                        {
+                            pattern[1] = true;
+                        }
                     }
                 }
 
                 // sound pattern 3
                 if (e.Y >= 106 && e.Y <= 126)
                 {
-                    if (sound3[(e.X - 15) / 15] == false)
+                    if (e.X <= 250)
                     {
-                        sound3[(e.X - 15) / 15] = true;
+                        if (sound3[(e.X - 15) / 15] == false)
+                        {
+                            sound3[(e.X - 15) / 15] = true;
+                        }
+                        else
+                        {
+                            sound3[(e.X - 15) / 15] = false;
+                        }
                     }
-                    else
+
+                    if (e.X >= 260 && e.X <= 280)
                     {
-                        sound3[(e.X - 15) / 15] = false;
+                        if (pattern[2] == true)
+                        {
+                            pattern[2] = false;
+                        }
+                        else
+                        {
+                            pattern[2] = true;
+                        }
+                    }
+                }
+
+                // sound pattern 4
+                if (e.Y >= 146 && e.Y <= 166)
+                {
+                    if (e.X <= 250)
+                    {
+                        if (sound4[(e.X - 15) / 15] == false)
+                        {
+                            sound4[(e.X - 15) / 15] = true;
+                        }
+                        else
+                        {
+                            sound4[(e.X - 15) / 15] = false;
+                        }
+                    }
+
+                    if (e.X >= 260 && e.X <= 280)
+                    {
+                        if (pattern[3] == true)
+                        {
+                            pattern[3] = false;
+                        }
+                        else
+                        {
+                            pattern[3] = true;
+                        }
                     }
                 }
 
